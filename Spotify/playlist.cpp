849b00d8e4418427df64f1568a436552c62221a5
Amirreza <arm186misaghim@gmail.com>
2026-07-11 17:04:03 +0330

    Playlist class created.

--- Spotify/playlist.cpp
+++ Spotify/playlist.cpp
@@ -1,3 +1,39 @@
 #include "playlist.h"
+#include <algorithm>
 
-Playlist::Playlist() {}
+Playlist::Playlist(): id(0), listenerId(0) {}
+
+
+Playlist::Playlist(int id , std::string name , int listenerID)
+    : id(id) , name(name) , listenerId(listenerID) {}
+
+int Playlist::getId()const {return id;}
+std::string Playlist::getName()const{return name;}
+int Playlist::getListenerId()const {return listenerId;}
+std::vector<int> Playlist::getSongIds()const{return songIds;}
+
+void Playlist::setId(int id){this->id = id;}
+void Playlist::setName(const std::string& name){this->name = name;}
+void Playlist::setListenerId(int listenerID){this->listenerId = listenerID;}
+void Playlist::setSongIds(const std::vector<int>& songIds){this->songIds = songIds;}
+
+void Playlist::addSong(int songId){
+    if(!hasSong(songId)){
+        songIds.push_back(songId);
+    }
+}
+
+void Playlist::removeSong(int songId){
+    auto it = std::find(songIds.begin(),songIds.end(),songId);
+    if(it != songIds.end()){
+        songIds.erase(it);
+    }
+}
+
+bool Playlist::hasSong(int songId)const{
+    return std::find(songIds.begin(),songIds.end(),songId)!=songIds.end();
+}
+
+int Playlist::getSongCount()const{
+    return songIds.size();
+}
--- Spotify/playlist.h
+++ Spotify/playlist.h
@@ -1,10 +1,35 @@
 #ifndef PLAYLIST_H
 #define PLAYLIST_H
 
+#include <string>
+#include <vector>
 class Playlist
 {
+private:
+    int id;
+    std::string name;
+    int listenerId;
+    std::vector<int> songIds;
+
 public:
     Playlist();
+    Playlist(int id , std::string name , int listenerId);
+
+    int getId()const;
+    std::string getName()const;
+    int getListenerId()const;
+    std::vector<int> getSongIds()const;
+
+    void setId(int id);
+    void setName(const std::string& name);
+    void setListenerId(int listenerId);
+    void setSongIds(const std::vector<int>& songIds);
+
+    void addSong(int songId);
+    void removeSong(int songId);
+    bool hasSong(int songId)const;
+    int getSongCount()const;
+
 };
 
 #endif // PLAYLIST_H
